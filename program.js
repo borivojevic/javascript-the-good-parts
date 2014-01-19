8c21a62c0960f63447d7fec2b0e32b6bf90c599b
Mirko Borivojevic <mirko@mirkoborivojevic.com>
2014-01-19 09:46:21 +0100

    Augmenting Types

--- program.js
+++ program.js
@@ -1,6 +1,17 @@
 document.writeln("Hello, world!");
 
 Function.prototype.method = function (name, func) {
-	this.prototype[bane] = func;
-	return this;
+	if (!this.prototype[name]) {
+		this.prototype[name] = func;
+		return this;
+	}
 };
+
+Number.method("integer", function() {
+	return Math[this < 0 ? "ceil" : "floor"](this);
+});
+
+String.method("trim", function() {
+	return this.replace(/^\s+|\s+$/g, "");
+});
+
